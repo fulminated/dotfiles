4e5eeb50b975f1aec13f15d6343a944286f50bc5
fulminated <fulminated@yahoo.com>
2015-07-12 21:07:51 +0530

    1010001 script

---
+++ scripts/binary.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+#  
+# Run this when you filming documentary on cyber crimes..XD. Oh
+# and be sure to use Green and black colorscheme to give it look of proper
+# haxor.
+#
+
+cat /dev/urandom | tr -cd ' 01'   
